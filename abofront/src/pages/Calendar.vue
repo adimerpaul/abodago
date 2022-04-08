b9a98d774f70f5d310e680c9ef63dba6a5b4416e
adimerpaul <adimer101@gmail.com>
2022-04-08 16:49:42 -0400

    proband

--- abofront/src/pages/Calendar.vue
+++ abofront/src/pages/Calendar.vue
@@ -261,7 +261,7 @@ export default {
          },
          agendar(){
            if(this.user.label=='')
-           this.user.id=0
+           //this.user.id=0
           this.$axios.post(process.env.API+'/agendar',{'actividad':this.actividad,'usuario_id':this.user.id,'fecha':this.fec,'hora':this.hr}).then(res=>{
             console.log(res.data)
             this.actividad=''
