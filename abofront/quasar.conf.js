f8695b4ca84160b6a09ff1211d2680cad7261237
adimerpaul <adimer101@gmail.com>
2023-01-12 20:08:46 -0400

    queado una vuena version

--- abofront/quasar.conf.js
+++ abofront/quasar.conf.js
@@ -73,7 +73,7 @@ module.exports = configure(function (ctx) {
       env: {
         API: ctx.dev
           ? 'http://localhost:8000/api'
-          : 'https://back.castilloygonzales.tk/api'
+          : 'https://bcastilloygonzales.siscenter.org/api'
       }
     },
 
